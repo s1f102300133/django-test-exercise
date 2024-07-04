d254b7fe9b694ebdf8b0ae52398d41a8f8da9c60
1f10230013 <s1f102300133@iniad.org>
2024-07-04 11:13:42 +0900

    修正一部 2

--- todo/views.py
+++ todo/views.py
@@ -24,11 +24,12 @@ def index(request):
                    due_at=make_aware (parse_datetime(request.POST['due_at'])))
         task.save()
         
-        if request.GET.get('order') == 'due':
+    if request.GET.get('order') == 'due':
             tasks = Task.objects.order_by('due_at')
-        else:
+    else:
             tasks = Task.objects.order_by('-posted_at')
-context = {
-    'tasks': tasks
+            
+    context = {
+        'tasks': tasks
     }
     return render (request, 'todo/index.html', context)
